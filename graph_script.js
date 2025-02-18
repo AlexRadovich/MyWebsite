7a5384f6db36fffbfeab8af450f1633ae9e63e9f
Alexander Radovich <alex@Alexanders-MacBook-Pro-4.local>
2025-02-18 14:25:20 -0500

    edge fundamentals created

--- graph_script.js
+++ graph_script.js
@@ -1,5 +1,7 @@
 let nodeCount = 0;
-let nodelist = []
+let nodelist = [];
+let node_positions = [];
+let node_position_found = 0;
 
 class Graph {
     createNode(x,y){
@@ -9,6 +11,10 @@ class Graph {
         console.log(created_node.name);
         const nodeElement = created_node.createElement();
         document.body.appendChild(nodeElement);
+    }
+    createEdge(x,y){
+        console.log("Node created");
+
     }
 
 }
@@ -52,13 +58,33 @@ function on_load() {
 function yell(){
     console.log("HERE")
 }
-
+function clicked_on_node(x,y){
+    node_position_found = 0;
+    for(let i = 0;i<nodelist.length-1;i++){
+        let x_pos = node_positions[i][0]
+        let y_pos = node_positions[i][1]
+        if((Math.abs(x-x_pos) <= 20) && (Math.abs(y-y_pos)<=20)){
+            console.log("POSITIONS:",x_pos,y_pos)
+            node_position_found = 1;
+        } 
+    }
+    return node_position_found;
+}
 document.body.addEventListener('click', (event) => {
     const x = event.clientX;
     const y = event.clientY;
-
-    // Use the NodeManager to create and add a new node
     theGraph.createNode(x, y);
+    node_positions.push([x,y])
+
+    if(clicked_on_node(x,y)){
+        yell()
+    }
+
+    console.log(node_positions[0]);
+    console.log(node_positions[1]);
+    console.log(node_positions[2]);
+    
+    
 });
 document.addEventListener('DOMContentLoaded', on_load());
 
